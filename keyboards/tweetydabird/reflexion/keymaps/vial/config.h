fb9824fc63ff38f85993a994504bc21fd1c30ebc
Markus Knutsson <markus.knutsson@tweety.se>
2026-03-08 10:25:02 +0100

    Reflexion X65
    Vial

---
+++ keyboards/tweetydabird/reflexion/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x74, 0x98, 0xDE, 0x84, 0x8A, 0xB8, 0xC5, 0x5C}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 1, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 7, 0 }
