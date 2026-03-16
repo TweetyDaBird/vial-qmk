9316d4121d54c9829f53d00304f723782fa4657e
Markus Knutsson <markus.knutsson@tweety.se>
2026-03-16 07:57:44 +0100

    reflexion vial corrected unlock

--- keyboards/tweetydabird/reflexion/keymaps/vial/config.h
+++ keyboards/tweetydabird/reflexion/keymaps/vial/config.h
@@ -5,4 +5,4 @@
 #define VIAL_KEYBOARD_UID {0x74, 0x98, 0xDE, 0x84, 0x8A, 0xB8, 0xC5, 0x5C}
 
 #define VIAL_UNLOCK_COMBO_ROWS { 1, 1 }
-#define VIAL_UNLOCK_COMBO_COLS { 0, 14 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
