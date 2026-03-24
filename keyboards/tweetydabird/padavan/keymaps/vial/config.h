d5cc0ca75e84772d9faf7ec31effebd4e9d790c4
Markus Knutsson <markus.knutsson@tweety.se>
2026-03-24 12:52:43 +0100

    initial vial

---
+++ keyboards/tweetydabird/padavan/keymaps/vial/config.h
@@ -0,0 +1,13 @@
+// Copyright 2026 Markus Knutsson (@TweetyDaBird)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x96, 0x8C, 0xF5, 0x19, 0xE7, 0xE6, 0x90, 0xFE}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
+#define VIAL_UNLOCK_COMBO_COLS {4, 4}
+
+// Static glow is ALWAYS on with RGBLIGHT
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
---
+++ keyboards/tweetydabird/padavan/keymaps/vial/rules.mk
@@ -0,0 +1,6 @@
+# Copyright 2026 Markus Knutsson (@TweetyDaBird)
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+# Needed for VIAL compatability (all are needed)
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
