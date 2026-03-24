d4f1e000b7f18eec22ad10984a32fdaf70905b2e
Markus Knutsson <markus.knutsson@tweety.se>
2026-03-24 14:22:57 +0100

    vial fixed unlock

--- keyboards/tweetydabird/padavan/keymaps/vial/config.h
+++ keyboards/tweetydabird/padavan/keymaps/vial/config.h
@@ -5,8 +5,8 @@
 
 #define VIAL_KEYBOARD_UID {0x96, 0x8C, 0xF5, 0x19, 0xE7, 0xE6, 0x90, 0xFE}
 
-#define VIAL_UNLOCK_COMBO_ROWS {0, 0}
-#define VIAL_UNLOCK_COMBO_COLS {4, 4}
+#define VIAL_UNLOCK_COMBO_ROWS {0, 4}
+#define VIAL_UNLOCK_COMBO_COLS {0, 4}
 
 // Static glow is ALWAYS on with RGBLIGHT
 #define RGBLIGHT_EFFECT_BREATHING
